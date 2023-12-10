8b39caa864e0dcd5b323466a199899270d097b10
alvaro <adelcastifo@gmail.com>
2023-12-10 17:34:05 +0100

    Kommentare , Dokumentation , sowie eine Erklärung des Codes mit einer passenden Analogie, wurden fertiggestellt für MessageAdapter ChatsDetailFragment.

--- app/src/main/java/c/s/w/adapter/MessageAdapter.kt
+++ app/src/main/java/c/s/w/adapter/MessageAdapter.kt
@@ -1,5 +1,6 @@
 package c.s.w.adapter
 
+// Import-Anweisungen für benötigte Klassen und Bibliotheken.
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,58 +9,222 @@ import androidx.recyclerview.widget.RecyclerView
 import com.syntax_institut.whatssyntax.R
 import com.syntax_institut.whatssyntax.data.model.Message
 
+
+/**
+ * MessageAdapter für die Darstellung von Nachrichten in einem RecyclerView.
+ *
+ * @param messages Eine Liste von Nachrichtenobjekten, die im RecyclerView angezeigt werden sollen.
+ */
 class MessageAdapter(private val messages: MutableList<Message>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    fun addMessage(message: Message) {
-        messages.add(message)
-        notifyItemInserted(messages.size - 1)
-    }
 
+    // Konstanten für die Unterscheidung von eingehenden und ausgehenden Nachrichten.
     companion object {
         const val VIEW_TYPE_INCOMING = 1
         const val VIEW_TYPE_OUTGOING = 2
     }
 
+    /**
+     * Bestimmt den Typ der Ansicht basierend auf der Position der Nachricht in der Liste.
+     *
+     * @param position Position der Nachricht in der Liste.
+     * @return Den View-Typ (eingehend oder ausgehend) für die Nachricht.
+     */
     override fun getItemViewType(position: Int): Int {
         return if (messages[position].incoming) VIEW_TYPE_INCOMING else VIEW_TYPE_OUTGOING
     }
 
+
+    /**
+     * Erstellt neue ViewHolder für eingehende oder ausgehende Nachrichten.
+     *
+     * @param parent Die ViewGroup, in der der neue ViewHolder gehalten wird.
+     * @param viewType Der Typ der Ansicht, die erstellt werden soll.
+     * @return Einen neuen ViewHolder für eingehende oder ausgehende Nachrichten.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        // LayoutInflater wird verwendet, um XML-Layouts in View-Objekte umzuwandeln.
         val layoutInflater = LayoutInflater.from(parent.context)
+
+        // Überprüft den Typ der Nachricht (eingehend oder ausgehend) und wählt das entsprechende
+        // Layout.
         return if (viewType == VIEW_TYPE_INCOMING) {
-            val view = layoutInflater.inflate(R.layout.item_message_incoming, parent, false)
+
+            // Lädt das Layout für eingehende Nachrichten und erstellt einen ViewHolder dafür.
+            val view = layoutInflater.inflate(
+                R.layout.item_message_incoming, parent,
+                false
+            )
             IncomingMessageViewHolder(view)
         } else {
-            val view = layoutInflater.inflate(R.layout.item_message_outgoing, parent, false)
+
+            // Lädt das Layout für ausgehende Nachrichten und erstellt einen ViewHolder dafür.
+            val view = layoutInflater.inflate(
+                R.layout.item_message_outgoing, parent,
+                false
+            )
             OutgoingMessageViewHolder(view)
         }
     }
 
+    /**
+     * Bindet die Nachrichtendaten an den entsprechenden ViewHolder.
+     *
+     * @param holder Der ViewHolder, der die Nachrichtendaten halten soll.
+     * @param position Die Position der Nachricht in der Liste.
+     */
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        // Holt die Nachricht an der gegebenen Position in der Liste.
         val message = messages[position]
+
+        // Überprüft den Typ des ViewHolders und bindet die Nachrichtendaten daran.
         if (holder is IncomingMessageViewHolder) {
+            // Für eingehende Nachrichten wird die bind-Methode des IncomingMessageViewHolder
+            // aufgerufen.
             holder.bind(message)
         } else if (holder is OutgoingMessageViewHolder) {
+            // Für ausgehende Nachrichten wird die bind-Methode des OutgoingMessageViewHolder
+            // aufgerufen.
             holder.bind(message)
         }
     }
 
+    /**
+     * Gibt die Gesamtanzahl der Nachrichten in der Liste zurück.
+     *
+     * @return Die Anzahl der Nachrichten in der Liste.
+     */
     override fun getItemCount() = messages.size
 
+
+    /**
+     * ViewHolder für eingehende Nachrichten.
+     *
+     * @param view Die Ansicht für eingehende Nachrichten.
+     */
     class IncomingMessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val messageTextView: TextView = view.findViewById(R.id.text_view_message_incoming)
 
+        /**
+         * Bindet eine eingehende Nachricht an die Ansicht.
+         *
+         * @param message Das Nachrichtenobjekt, das an die Ansicht gebunden werden soll.
+         */
         fun bind(message: Message) {
             messageTextView.text = message.text
         }
     }
 
+    /**
+     * ViewHolder für ausgehende Nachrichten.
+     *
+     * @param view Die Ansicht für ausgehende Nachrichten.
+     */
     class OutgoingMessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val messageTextView: TextView = view.findViewById(R.id.text_view_message_outgoing)
 
+        /**
+         * Bindet eine ausgehende Nachricht an die Ansicht.
+         *
+         * @param message Das Nachrichtenobjekt, das an die Ansicht gebunden werden soll.
+         */
         fun bind(message: Message) {
             messageTextView.text = message.text
         }
     }
 }
+
+/**
+ * Zusammenfassung des MessageAdapter-Codes
+ * Der MessageAdapter ist ein spezialisierter Adapter für die RecyclerView, der in deinem
+ * ChatsDetailFragment verwendet wird, um Nachrichten anzuzeigen. Der Adapter unterscheidet zwischen
+ * eingehenden und ausgehenden Nachrichten und zeigt sie entsprechend an. Die Schlüsselmethoden und
+ * ihre Funktionen sind:
+ *
+ * getItemViewType: Bestimmt, ob eine Nachricht eingehend oder ausgehend ist, basierend auf ihrer
+ *                  Position in der Nachrichtenliste.
+ *
+ * onCreateViewHolder: Erstellt neue ViewHolder für jede Nachricht, je nachdem, ob sie eingehend
+ *                     oder ausgehend ist.
+ *
+ * onBindViewHolder: Bindet die Nachrichtendaten an den entsprechenden ViewHolder.
+ *
+ * getItemCount: Gibt die Gesamtzahl der Nachrichten in der Liste zurück.
+ *
+ * IncomingMessageViewHolder und OutgoingMessageViewHolder: Zwei interne Klassen, die die
+ *                                                          Darstellung für eingehende bzw.
+ *                                                          ausgehende Nachrichten definieren.
+ *
+ * Analogie zu jeder Methode
+ * getItemViewType: Stell dir vor, du sortierst Briefe in deinem Postfach. Du prüfst jeden Brief
+ *                 (Nachricht), um festzustellen, ob er von jemand anderem gesendet wurde
+ *                 (eingehend) oder von dir selbst (ausgehend).
+ *
+ * onCreateViewHolder: Dies ist wie das Auswählen eines Briefumschlags für deine Briefe.
+ *                     Abhängig davon, ob es ein eingehender oder ausgehender Brief ist,
+ *                     wählst du einen blauen oder roten Umschlag (entsprechendes Layout).
+ *
+ * onBindViewHolder: Hier legst du den Brief (Nachricht) in den ausgewählten Umschlag.
+ *                   Der Umschlag (ViewHolder) präsentiert den Inhalt des Briefes auf die
+ *                   richtige Weise.
+ *
+ * getItemCount: Das ist, als würdest du zählen, wie viele Briefe du in deinem Postfach hast.
+ *
+ * IncomingMessageViewHolder und OutgoingMessageViewHolder: Diese sind vergleichbar mit
+ *                                                          spezialisierten Briefträgern.
+ *                                                          Einer kümmert sich nur um eingehende,
+ *                                                          der andere nur um ausgehende Briefe,
+ *                                                          und jeder weiß genau, wie er seinen
+ *                                                          jeweiligen Brieftyp behandeln muss.
+ *
+ *  Was hat es mit dem Companion Objekt auf sich ?
+ *
+ *   Ein `Companion Object` ist ein spezieller Typ von Objekt, das innerhalb einer Klasse definiert
+ *   wird und dessen Eigenschaften und Funktionen ohne eine Instanz dieser Klasse aufgerufen werden
+ *   können.
+ *
+ *
+ *   In deinem `MessageAdapter`-Code wurden Companion Objects folgendermaßen verwendet:
+ *
+ * ```kotlin
+ * companion object {
+ *     const val VIEW_TYPE_INCOMING = 1
+ *     const val VIEW_TYPE_OUTGOING = 2
+ * }
+ * ```
+ *
+ * Hier definierst du zwei Konstanten (`VIEW_TYPE_INCOMING` und `VIEW_TYPE_OUTGOING`), die
+ * verwendet werden, um zu bestimmen, ob eine Nachricht eingehend oder ausgehend ist. Da diese
+ * Konstanten innerhalb eines Companion Objects definiert sind, können sie überall innerhalb
+ * deiner Klasse ohne die Notwendigkeit einer Instanz der `MessageAdapter`-Klasse verwendet werden.
+ *
+ *  Vorteile von Companion Objects
+ *
+ * 1. Klassenweite Felder und Methoden: Companion Objects ermöglichen es dir, Felder und
+ *                                          Methoden zu definieren, die auf Klassenebene und
+ *                                          nicht auf Instanzebene vorhanden sind. Das bedeutet,
+ *                                          sie gehören zur Klasse selbst und nicht zu einem
+ *                                          bestimmten Objekt dieser Klasse.
+ *
+ * 2. Einfacher Zugriff: Werte und Funktionen innerhalb eines Companion Objects können direkt über
+ *                       den Klassennamen aufgerufen werden, was den Zugriff vereinfacht und klarer
+ *                       macht, dass es sich um klassenweite Eigenschaften handelt.
+ *
+ * 3. Implementierung von Interfaces: Ein Companion Object kann ein Interface implementieren,
+ *                                    was bei statischen Methoden in Java nicht möglich ist.
+ *                                    Das ermöglicht es Companion Objects, sich an bestimmte
+ *                                    Schnittstellen zu halten und bietet mehr Flexibilität
+ *                                    in der Verwendung.
+ *
+ * 4. **Singleton-Muster**: Da es von jedem Companion Object nur eine einzige Instanz gibt, eignen
+ *                          sie sich gut für die Umsetzung des Singleton-Designmusters in Kotlin.
+ *
+ * In meinem speziellen Fall ist der Hauptvorteil der Verwendung des Companion Objects die
+ * klare und saubere Organisation deiner Code-Basis. Die Konstanten "VIEW_TYPE_INCOMING" und
+ * "VIEW_TYPE_OUTGOING" sind eng mit der "MessageAdapter"-Klasse verbunden, aber da sie allgemein
+ * und unveränderlich sind, macht es Sinn, sie als Teil eines Companion Objects zu definieren.
+ *
+ */
--- app/src/main/java/com/syntax_institut/whatssyntax/ChatsDetailFragment.kt
+++ app/src/main/java/com/syntax_institut/whatssyntax/ChatsDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.syntax_institut.whatssyntax
 
+// Import-Anweisungen für benötigte Klassen und Bibliotheken.
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,57 +14,79 @@ import androidx.recyclerview.widget.RecyclerView
 import c.s.w.adapter.MessageAdapter
 import com.syntax_institut.whatssyntax.data.model.Message
 
+
+/**
+ * ChatsDetailFragment dient zur Anzeige der Nachrichten eines spezifischen Chats.
+ */
 class ChatsDetailFragment : Fragment() {
 
+    // Deklaration der benötigten UI-Komponenten und Adapter.
     private lateinit var messageAdapter: MessageAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var sendMessageButton: Button
     private lateinit var messageEditText: EditText
 
+    /**
+     * onCreateView wird aufgerufen, um das Layout für das Fragment zu erstellen.
+     *
+     * @param inflater LayoutInflater-Objekt, das zum Aufblasen des Layouts verwendet wird.
+     * @param container Container für das Fragment.
+     * @param savedInstanceState Bundle, das den Zustand des Fragments speichert.
+     * @return Die erstellte View für das Fragment.
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_chats_detail, container, false)
 
-        // Initialisiere deine RecyclerView
+        // Layout für das Fragment aufblasen.
+        val view = inflater.inflate(
+            R.layout.fragment_chats_detail, container,
+            false
+        )
+
+        // Initialisierung des RecyclerView für Nachrichten.
         recyclerView = view.findViewById(R.id.recycler_view_messages)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        // Hole die chatId aus den SafeArgs
+        // Zugriff auf die MainActivity und deren Datenquelle.
         val mainActivity = activity as MainActivity
         val dataSource = mainActivity.datasource
+
+        // Empfangen der Argumente, die an das Fragment übergeben wurden.
         val args: ChatsDetailFragmentArgs by navArgs()
         val chatIndex = args.chatIndex
 
-        // Hole die Nachrichten für diesen Chat
+        // Nachrichten für den spezifischen Chat aus der Datenquelle abrufen.
         val messages = dataSource.getMessagesForChat(chatIndex.toInt())
 
-        // Initialisiere den Adapter mit den Nachrichten
+        // Initialisierung des MessageAdapters und Setzen als Adapter des RecyclerView.
         messageAdapter = MessageAdapter(messages)
         recyclerView.adapter = messageAdapter
 
-        // Initialisiere den Senden-Button und EditText
+        // Initialisierung der UI-Komponenten für das Senden von Nachrichten.
         sendMessageButton = view.findViewById(R.id.button_chatbox_send)
         messageEditText = view.findViewById(R.id.edittext_chatbox)
 
+        // Setzen eines ClickListeners auf den Senden-Button.
         sendMessageButton.setOnClickListener {
             val messageText = messageEditText.text.toString()
             if (messageText.isNotEmpty()) {
-                // Füge die neue Nachricht hinzu
-                val newMessage = Message(messageText, incoming = false) // 'incoming' basierend auf deiner Logik setzen
+
+                // Erstellung und Hinzufügen einer neuen Nachricht zur Datenquelle.
+                val newMessage = Message(messageText, incoming = false)
                 dataSource.addMessageToChat(chatIndex.toInt(), newMessage)
 
 
-
-                val messageList = dataSource.getMessagesForChat(chatIndex.toInt())
+                // Aktualisierung des MessageAdapters und der RecyclerView.
+                val messageList = dataSource.getMessagesForChat(
+                    chatIndex.toInt()
+                )
                 messageAdapter.notifyItemInserted(messageList.size - 1)
 
-                // Clear the input box after sending
+                // Leeren des Eingabefelds und Scrollen zum Ende der Nachrichtenliste.
                 messageEditText.text.clear()
-                // Scroll to the bottom to show the latest message
-                recyclerView.scrollToPosition(messageList.size - 1)// Scroll to the bottom to show the latest message
+                recyclerView.scrollToPosition(messageList.size - 1)
             }
         }
 
@@ -72,3 +95,43 @@ class ChatsDetailFragment : Fragment() {
 
 
 }
+
+/**
+ *Zusammenfassung des ChatsDetailFragment-Codes
+ * Das ChatsDetailFragment ist für die Anzeige und Interaktion innerhalb eines spezifischen
+ * Chat-Fensters in deiner Messaging-App zuständig. Es enthält Elemente zum Anzeigen von Nachrichten
+ * und zur Eingabe neuer Nachrichten. Der Hauptteil des Codes umfasst:
+ *
+ * Layout-Initialisierung (onCreateView): Hier wird das Layout für das Detail-Chat-Fenster erstellt,
+ *                                        einschließlich eines RecyclerView für die Nachrichten und
+ *                                        eines EditText zum Eingeben neuer Nachrichten.
+ *
+ * Nachrichten-Laden und Adapter-Initialisierung: Der Code lädt die Nachrichten für einen
+ *                                                spezifischen Chat und initialisiert den
+ *                                                MessageAdapter für den RecyclerView, der für die
+ *                                                Darstellung der Nachrichten zuständig ist.
+ *
+ * Nachrichten-Senden-Logik: Ein Button für das Senden von Nachrichten wird mit einem
+ *                           OnClickListener versehen, der neue Nachrichten zur Nachrichtenliste
+ *                           hinzufügt und die Ansicht aktualisiert.
+ *
+ * Analogie zu jeder Komponente
+ * Layout-Initialisierung (onCreateView): Stelle dir vor, du betrittst ein Postamt, um einen Brief
+ *                                        zu verschicken. Das Postamt (ChatsDetailFragment) hat
+ *                                        Schalter (UI-Elemente) für das Schreiben (Eingabefeld) und
+ *                                        das Anzeigen von bereits versendeten Briefen
+ *                                        (Nachrichten-Liste).
+ *
+ * Nachrichten-Laden und Adapter-Initialisierung: Das ist vergleichbar mit dem Öffnen eines
+ *                                                Postfachs, um zu sehen, welche Briefe
+ *                                                (Nachrichten) du bereits erhalten hast.
+ *                                                Der MessageAdapter ist wie der Postbeamte,
+ *                                                der entscheidet, wie die Briefe in deinem
+ *                                                Fach angeordnet werden.
+ *
+ * Nachrichten-Senden-Logik: Das Senden einer Nachricht ist wie das Schreiben eines Briefes und das
+ *                           Einwerfen in den Briefkasten. Der sendMessageButton ist der
+ *                           Briefkasten. Wenn du einen Brief (Nachricht) einwirfst, fügt der
+ *                           Postbeamte (Adapter) diesen zu deinen anderen Briefen hinzu und zeigt
+ *                           dir das aktualisierte Postfach an.
+ */
